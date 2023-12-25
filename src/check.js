ab1a173bda7f025f2fc3f4f78c22968f7fd219ef
User <User@DESKTOP-C51DDIB>
2023-12-25 12:37:07 +0500

    remove from list

---
+++ src/check.js
@@ -0,0 +1,6 @@
+const { Node } = require('../extensions/list-tree.js');
+const { BinarySearchTree } = require('./binary-search-tree.js');
+
+let bst = new BinarySearchTree();
+
+console.log(bst.root());
